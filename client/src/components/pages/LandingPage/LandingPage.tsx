805bbd43fed11012ea5d63660ebfb166569a80f9
Alex Dimitrov <ad@auxnederlandbv.nl>
2024-08-04 11:47:13 +0300

    setup done

---
+++ client/src/components/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LandingPage.css';
+
+function LandingPage() {
+  return (
+    <div className="LandingPage">
+      hi
+    </div>
+  );
+}
+
+export default LandingPage;
